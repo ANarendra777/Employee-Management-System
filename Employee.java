aa3bf87774724f0e350786f4794e7ea8b6817d75
ANarendra777 <amarnarenz777@gmail.com>
2026-06-19 11:17:51 +0530

    Added Department

--- Employee.java
+++ Employee.java
@@ -5,10 +5,12 @@ class Employee {
         int id = 101;
         String name = "John";
         double salary = 50000;
+        String department = "IT"; 
 
         System.out.println("Employee ID : " + id);
         System.out.println("Employee Name : " + name);
         System.out.println("Salary : " + salary);
+        System.out.println("Department : " + department);
 
     }
 }
