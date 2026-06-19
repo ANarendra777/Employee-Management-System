0302dd158be798eb0e64167ded3fe9fce85de7f3
ANarendra777 <amarnarenz777@gmail.com>
2026-06-19 11:05:25 +0530

    Created Employee Java Program

---
+++ Employee.java
@@ -0,0 +1,14 @@
+class Employee {
+
+    public static void main(String args[]) {
+
+        int id = 101;
+        String name = "John";
+        double salary = 50000;
+
+        System.out.println("Employee ID : " + id);
+        System.out.println("Employee Name : " + name);
+        System.out.println("Salary : " + salary);
+
+    }
+}
